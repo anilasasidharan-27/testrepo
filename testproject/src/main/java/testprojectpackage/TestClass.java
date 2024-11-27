d851223a452fdc6fef500f87a5b82c794a25c96d
anila <anila@GokulamPC>
2024-11-27 19:42:34 +0530

    commit

---
+++ testproject/src/main/java/testprojectpackage/TestClass.java
@@ -0,0 +1,16 @@
+package testprojectpackage;
+
+public class TestClass {
+
+	public void display()
+	{
+		System.out.println("Hello world");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestClass obj=new TestClass();
+		obj.display();
+
+	}
+
+}
